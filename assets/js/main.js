c45822ee744dc51d4c69036cedd4ee62f7cc5cde
Jean-Luc <jean-luc.a@ideafactory.fr>
2020-11-23 14:43:51 +0100

    Fetch LotOfThing api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,18 @@
+const headers = new Headers()
+
 (() => {
     console.log(`Hi all, i'm working`)
 
     // Well, load the template till app load
+    fetch (
+        `https://localhost/api/v2/loadOfThings`,
+        {
+            method: 'get',
+            headers
+        }
+    ).then ((result) => result.json())
+    .then((data) => {
+
+    })
 })()
+
