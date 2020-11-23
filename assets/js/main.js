39e9e465c7e0e59520fc115b6523c03390e8dd93
Jean-Luc <jean-luc.a@ideafactory.fr>
2020-11-23 14:40:43 +0100

    Add UI for application loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,20 @@
+const headers = new Headers()
+headers.append('Content-Type:', 'application/json')
+
 (() => {
     console.log(`Hi all, i'm working`)
 
     // Well, load the template till app load
+    fetch (
+        `https://localhost/api/v2/loadOfThings`,
+        {
+            method: 'get',
+            headers: {
+
+            }
+        }
+    ).then ((result) => result.json())
+    .then((data) => {
+        
+    })
 })()
